f8fb301d249b6be95a0432889720d9766dd5ed2c
Choi Hanna <gkssk4163@hanmail.net>
2022-08-25 14:44:18 +0900

    feat: 사원관리 페이지 구현 - 사원관리 페이지 틀 및 기본기능 구현

---
+++ src/main/java/com/jeo/web/comp/controller/CompController.java
@@ -0,0 +1,21 @@
+package com.jeo.web.comp.controller;
+
+import com.jeo.webapp.comp.service.CompService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CompController {
+	@Autowired
+	CompService compService;
+
+	@GetMapping(value = "/comp")
+	public String compGET() {
+		return "redirect:/comp/user";
+	}
+
+	@GetMapping(value = "/comp/user")
+	public void compUserGET() {
+	}
+}
